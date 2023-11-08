3d352c2a6da9c78c4210fb66caf1a46aeb311aa5
Michelle Duso <chelleduso@gmail.com>
2023-11-07 21:09:21 -0300

    Instalações e importações

---
+++ vendas.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+# importar a base de dados
+
+# visualizar a base de dados
+
+# faturamento por loja
+
+# quantidade de produtos vendidos por loja
+
+# ticket médio por produto em cada loja
+
+# enviar um email com o relatório
+
