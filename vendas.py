6e145de49b3c812be9295dc7f0589e434135f324
Michelle Duso <chelleduso@gmail.com>
2023-11-14 20:30:17 -0300

    Configuração do e-mail e formação

--- vendas.py
+++ vendas.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import win32com.client as win32
+
 
 # importar a base de dados
 tabela_vendas = pd.read_excel('Vendas.xlsx')
@@ -17,8 +19,38 @@ print(quantidade)
 print('-' * 50)
 # ticket médio por produto em cada loja
 ticket_medio = (faturamento['Valor Final'] / quantidade['Quantidade']).to_frame()
+ticket_medio = ticket_medio.rename(columns={0: 'Ticke Médio'})
 print(ticket_medio)
 
 
 # enviar um email com o relatório
+
+outlook = win32.Dispatch('outlook.application')
+mail = outlook.CreateItem(0)
+mail.To = 'michelle.duso@hotmail.com'
+mail.Subject = 'Relatório de Vendas por Loja'
+mail.HTMLBody = f'''
+<p>Prezados,</p>
+
+<p>Segue o Relatório de Vendas por cada loja.</p>
+
+<p>Faturamento:</p>
+{faturamento.to_html(formatters={'Valor Final': 'R${:,.2f}'.format})}
+
+<p>Quantidade Vendida:</p>
+{quantidade.to_html(formatters={'Quantidade Vendida': 'R${:,.2f}'.format})}
+
+<p>Ticket Médio dos Produtos em cada Loja.</p>
+{ticket_medio.to_html(formatters={'Ticket Médio': 'R${:,.2f}'.format})}
+
+<p>Qualquer dúvida estou a disposição.</p>
+
+<p>Att.,</p>
+<p>Michelle</p>
+
+'''
+
+mail.Send()
+
+
 
